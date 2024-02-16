fe32d1b69a84bd9f549410e53d055b9495e6c4bf
Shabih Alam <96066877+mailingtoshabih@users.noreply.github.com>
2024-02-17 02:07:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const mongoDBUri = process.env.MONGODB_URI;
 
 
-mongoose.connect(`mongodb+srv://alamshabih3:8091@cluster0.pa97m3m.mongodb.net/quiz`)
+mongoose.connect(mongoDBUri)
     .then(() => console.log("Server Connected..."))
     .catch((e) => console.log(e.message))
 
